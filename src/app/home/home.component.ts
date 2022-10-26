5a60f25d198cc159bc65bf97ac2cec1d4c0fdf98
Христюк Дарья Алексеевна <@itstep.loc>
2022-10-26 18:08:15 +0600

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { collection, getDocs, doc, getDoc, addDoc,deleteDoc  } from "firebase/firestore";
+import { db } from 'src/utils/firebase';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,38 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+async ngOnInit(): Promise<void> {
+  // await deleteDoc(doc(db, "products", "1OSCVrqtOYKv3qk9UB0p"));  
 
+  // const docRef = await addDoc(collection(db, "products"), {
+  //   id: "Phone 3",
+  //   title: "Phone 3",
+  //   price: 700,
+  //   category: "Phones",
+  //   spec: {
+  //     RAM: 6,
+  //     ROM:258
+  //   }
+  // });
+  // console.log("Document written with ID: ", docRef.id);
+
+
+//     const docRef = doc(db, "products", "Phone 1");
+// const docSnap = await getDoc(docRef);
+
+// if (docSnap.exists()) {
+//   console.log("Document data:", docSnap.data());
+// } else {
+//   // doc.data() will be undefined in this case
+//   console.log("No such document!");
+// }
+  }
+  
+  async getDocuments(document: string){
+  const querySnapshot = await getDocs(collection(db, document));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+}
 }
---
+++ src/utils/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-E_8UUGbvyNMTuUH_PsvOhr-U284Ghkw",
+  authDomain: "angular-exam-58035.firebaseapp.com",
+  databaseURL: "https://angular-exam-58035-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "angular-exam-58035",
+  storageBucket: "angular-exam-58035.appspot.com",
+  messagingSenderId: "774191161402",
+  appId: "1:774191161402:web:d7f06cf97cd61c69831503",
+  measurementId: "G-N36G9EMYLP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+// export default {
+//     db,
+//     analytics
+// }
